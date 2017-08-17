8e5635504f964aa01008a3d7599c5b71a0a575fa
Garima Singh <igarimasingh@gmail.com>
2017-08-17 17:23:01 +0800

    Adds home & snippet controller

--- src/main/java/io/pivotal/workshop/Application.java
+++ src/main/java/io/pivotal/workshop/Application.java
@@ -2,6 +2,13 @@ package io.pivotal.workshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 
 @SpringBootApplication
 public class Application {
@@ -9,3 +16,32 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
+
+//
+//@Component
+//interface PersonRepository extends JpaRepository<Person, Integer> {
+//
+//}
+//
+//@Entity
+//class Person {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private Long id;
+//    private String firstName;
+//    private String lastName;
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public Person(String firstName, String lastName) {
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//    }
+//}
--- src/main/java/io/pivotal/workshop/controller/IndexController.java
+++ src/main/java/io/pivotal/workshop/controller/IndexController.java
@@ -1,4 +1,4 @@
-package io.pivotal.workshop;
+package io.pivotal.workshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
